2735ac85bb476c07f31811c1efa397c93a8e9aba
Luca Dünte <luca@duente-family.de>
2024-09-21 15:08:12 +0200

    Added User Mapping

--- src/includes/class.php
+++ src/includes/class.php
@@ -137,3 +137,89 @@ class ksso_data {
         }
     }
 }
+
+class ksso_roles {
+    private $roles = array();
+
+    public function __construct() 
+    {
+        $this->write_log('ksso_roles::__construct()');
+        $this->load_rolemapping_from_db();
+    }
+
+    public function add_rolemapping($kc_role, $wp_role) 
+    {
+        $this->write_log('ksso_roles::add_rolemapping() - ' . $kc_role . ' - ' . $wp_role);
+        $kc_role = sanitize_text_field($kc_role);
+        $wp_role = sanitize_text_field($wp_role);
+
+        if (strlen($kc_role) == 0 || strlen($wp_role) == 0 || strlen($kc_role) > 255 || strlen($wp_role) > 255) 
+        {
+            throw new Exception('Role names must be between 1 and 255 characters.');
+        }
+
+        if (!isset($wp_roles->roles[$wp_role])) 
+        {
+            throw new Exception('Role does not exist.');
+        }
+
+        $this->add_rolemapping_to_db($kc_role, $wp_role);
+    }
+
+    public function user_add_rolemapping($userid, $user_jwt_token)
+    {
+        $this->write_log('ksso_roles::user_add_rolemapping() - ' . $userid . ' - ' . print_r($user_jwt_token, true));
+        $userroles = $user_jwt_token->realm_access->roles;
+        $size = count($userroles);
+
+        if (sizeof($this->roles) == 0) 
+        {
+            return;
+        }
+
+        for ($i = 0; $i < $size; $i++) 
+        {
+            $role = $userroles[$i];
+            if (array_key_exists($role, $this->roles)) 
+            {
+                $wp_role = $this->roles[$role];
+                $user = new WP_User($userid);
+                $user->set_role($wp_role);
+            }
+        }
+    }
+
+    private function add_rolemapping_to_db($kc_role, $wp_role) 
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'ksso_roles';
+
+        $wpdb->insert($table_name, array('keycloal_role_name' => $kc_role, 'wordpress_role_name' => $wp_role));
+        $this->load_rolemapping_from_db();
+    }
+
+    private function load_rolemapping_from_db() 
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'ksso_roles';
+
+        $results = $wpdb->get_results("SELECT * FROM $table_name");
+
+        if (empty($results)) 
+        {
+            return;
+        }
+
+        foreach ($results as $result) 
+        {
+            $this->roles[$result->keycloak_role_name] = $result->wordpress_role_name;
+        }
+    }
+
+    private function write_log($message)
+    {
+        $log_file = WP_CONTENT_DIR . '/log/sso.log';
+        file_put_contents($log_file, date("Y-m-d H:i:s") . " - " . $message . PHP_EOL, FILE_APPEND);
+
+    }
+}
--- src/keycloaksso.php
+++ src/keycloaksso.php
@@ -13,6 +13,7 @@
  */
 
  require_once plugin_dir_path(__FILE__) . 'includes/class.php';
+ require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
 
 
  if (!function_exists('ksso_deploy_plugin')) {
@@ -28,6 +29,19 @@
             realm varchar(255) NOT NULL,
             clientid varchar(255) NOT NULL,
             clientsecret text NOT NULL,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+
+        $table_name = $wpdb->prefix . 'ksso_roles';
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = "CREATE TABLE $table_name (
+            id TINYINT(1) NOT NULL,
+            keycloak_role_name varchar(255) NOT NULL,
+            wordpress_role_name varchar(255) NOT NULL,
             PRIMARY KEY  (id)
         ) $charset_collate;";
 
@@ -51,265 +65,6 @@
     add_filter('query_vars', 'ksso_register_query_vars');
 }
 
-
-function ksso_request_jwt($code, $tokenservice, $clientid, $clientsecret) 
-{
-    try 
-    {
-        $params = array(
-            'grant_type'    => 'authorization_code',
-            'code'          => $code,
-            'redirect_uri'  => site_url() . '/sso/login', 
-            'client_id'     => $clientid, 
-            'client_secret' => $clientsecret  
-        );
-
-        $tokenendpoint = $tokenservice . '/token';
-
-        $response = wp_remote_post($tokenendpoint, array(
-            'method' => 'POST',
-            'body' => $params
-        ));
-
-        if (is_wp_error($response)) 
-        {
-            throw new Exception('Request failed: ' . $response->get_error_message());
-        }
-
-        $status_code = wp_remote_retrieve_response_code($response);
-        $body = wp_remote_retrieve_body($response);
-
-        if ($status_code == 200) 
-        {
-            $response_data = json_decode($body, true);
-
-            if (isset($response_data['access_token'])) 
-            {
-                return $response_data['access_token']; 
-            } 
-            else 
-            {
-                throw new Exception('No access token found in response.');
-            }
-        }
-        else 
-        {
-            throw new Exception("Error: Status - $status_code, Message - $body");
-        }
-
-    } 
-    catch (Exception $e) 
-    {
-        throw new Exception('A critical Login error has occurred. Please inform the server administrator and report this error: ' . $e->getMessage());
-    }
-}
-
-
-
-function ksso_decode_verify_read_jwt($jwt, $publickey, $session) 
-{
-    list($header, $payload, $signature) = explode('.', $jwt);
-
-    $decoded_header = json_decode(base64_decode($header));
-    $decoded_payload = json_decode(base64_decode($payload));
-
-    if (!$decoded_header || !$decoded_payload) 
-    {
-        throw new Exception('Failed to decode JWT header or payload.');
-    }
-
-    if ($decoded_header->alg !== 'RS256') 
-    {
-        throw new Exception('Unexpected algorithm: ' . $decoded_header->alg);
-    }
-
-    $publickey = "-----BEGIN PUBLIC KEY-----\n" . wordwrap($publickey, 64, "\n", true) . "\n-----END PUBLIC KEY-----";
-    $publickey_resource = openssl_pkey_get_public($publickey);
-
-    if (!$publickey_resource) 
-    {
-        throw new Exception('Invalid public key format.');
-    }
-
-    $data_to_verify = $header . '.' . $payload;
-    $signature_decoded = base64_decode(strtr($signature, '-_', '+/'));
-
-    if (!$signature_decoded)
-    {
-        throw new Exception('Failed to decode the JWT signature.');
-    }
-
-    $verified = openssl_verify($data_to_verify, $signature_decoded, $publickey_resource, OPENSSL_ALGO_SHA256);
-
-    if ($verified === 1) 
-    {
-        if (isset($decoded_payload->exp) ) 
-        {
-            $current_time = time();
-            if ($decoded_payload->exp < $current_time) 
-            {
-                throw new Exception('Token has expired.');
-            }
-        } 
-        else 
-        {
-            throw new Exception('Token has no expiration date.');
-        }
-
-        if (!isset($decoded_payload->session_state) || $decoded_payload->session_state !== $session) 
-        {
-            throw new Exception('Session state does not match.');
-        }
-
-        return $decoded_payload;
-    } 
-    elseif ($verified === 0) 
-    {
-        throw new Exception('Invalid signature.');
-    } 
-    else 
-    {
-        throw new Exception('Error verifying token: ' . openssl_error_string());
-    }
-}
-
-function ksso_wordpress_user($userdata)
-{
-    try
-    {
-        $loginuserurl = false;
-
-        $user = get_user_by('email', $userdata->email);
-
-        if (!$user) 
-        {
-            $data = array(
-                'user_login' => $userdata->preferred_username,
-                'user_email' => $userdata->email,
-                'user_pass' => wp_generate_password(),
-                'first_name' => $userdata->given_name,
-                'last_name' => $userdata->family_name,
-            );
-            $userID = wp_insert_user($data);
-
-            if (is_wp_error($userID)) 
-            {
-                throw new Exception('Error creating user: ' . $userID->get_error_message());
-            }
-
-            $datausersub = array(
-                'user_id' => $userID,
-                'sub' => $userdata->sub,
-            );
-
-            $usermeta = add_user_meta($userID, 'sub', $userdata->sub, true);
-            if (is_wp_error($usermeta)) 
-            {
-                throw new Exception('Error adding user meta (sub): ' . $usermeta->get_error_message());
-            }
-            return $userID;
-
-        }
-        else 
-        {
-            $usersub = get_user_meta($user->ID, 'sub', true);
-
-            if ($usersub !== $userdata->sub && $user->user_email === $userdata->email) 
-            {
-                // Es melder sich ein Nutzer an, der wp_die selbe E-Mail-Adresse wie ein bereits bestehender Nutzer in der Wordpress Datenbank hat. Allerdings unterscheidet sich wp_die Sub-ID der beiden Nutzer. 
-                // Das heißt, dass der alte Nutzer im Quellsystem gelöscht wurde und ein neuer Nutzer mit der selben E-Mail-Adresse angelegt wurde. In wp_diesem Fall wird der alte Nutzer 
-                // in der Wordpress Datenbank gelöscht und der neue Nutzer wird angelegt.
-                
-                wp_delete_user($user->ID);
-
-                if (is_wp_error($user)) 
-                {
-                    throw new Exception('Error deleting user (Sub doesnt match): ' . $user->get_error_message());
-                }
-
-                $data = array(
-                    'user_login' => $userdata->preferred_username,
-                    'user_email' => $userdata->email,
-                    'user_pass' => wp_generate_password(),
-                    'first_name' => $userdata->given_name,
-                    'last_name' => $userdata->family_name,
-                );
-                $user = wp_insert_user($data);
-
-                if (is_wp_error($user)) 
-                {
-                    throw new Exception('Error creating user: ' . $user->get_error_message());
-                }
-    
-                $datausersub = array(
-                    'user_id' => $user->ID,
-                    'sub' => $userdata->sub,
-                );
-    
-                $usermeta = add_user_meta($user, 'sub', $userdata->sub, true);
-                if (is_wp_error($usermeta)) 
-                {
-                    throw new Exception('Error adding user meta (sub): ' . $usermeta->get_error_message());
-                }
-
-                return $user->ID;
-            }
-            else
-            {
-                $userID = $user->ID;
-                $data = array(
-                    'ID' => $userID,
-                    'user_login' => $userdata->preferred_username,
-                    'user_email' => $userdata->email,
-                    'user_pass' => wp_generate_password(),
-                    'first_name' => $userdata->given_name,
-                    'last_name' => $userdata->family_name,
-                );
-                $user = wp_update_user($data);
-    
-                if (is_wp_error($user)) 
-                {
-                    throw new Exception('Error updating user: ' . $user->get_error_message());
-                }
-
-                return $userID;
-            }
-        }
-    }
-    catch (Exception $e) 
-    {
-        throw new Exception('A critical error has occurred. Please inform the server administrator and report this error: ' . $e->getMessage());
-    }
-}
-
-function ksso_userlogin($userid)
-{
-    wp_set_current_user($userid);
-    
-    wp_set_auth_cookie($userid);
-    
-    do_action('wp_login', get_user_by('id', $userid)->user_login, get_user_by('id', $userid));
-    
-    wp_safe_redirect(home_url());
-
-    exit;
-}
-
-
-// function ksso_add_user_to_db($userdata)
-// {
-//     global $wp_roles;
-//     if (!isset($wp_roles)) 
-//     {
-//         $wp_roles = new WP_Roles();
-//     }
-//     $roles = $wp_roles->get_names();
-
-
-
-
-// }
-
 function ksso_handle_sso_login() 
 {
     if (get_query_var('sso_login')) 
@@ -317,6 +72,7 @@ function ksso_handle_sso_login()
         try 
         {
             $kcdata = new ksso_data();
+            $kcroles = new ksso_roles();
 
             $iss = isset($_GET['iss']) ? sanitize_text_field($_GET['iss']) : null;
             $code = isset($_GET['code']) ? sanitize_text_field($_GET['code']) : null;
@@ -347,6 +103,8 @@ function ksso_handle_sso_login()
             $payload = ksso_decode_verify_read_jwt($access_token, $kcdata->get_publickey(), $session);
 
             $userid = ksso_wordpress_user($payload);
+
+            $kcroles->user_add_rolemapping($userid, $payload);
 
             ksso_userlogin($userid);
 
